5b5d4a63ea74da68686a92d5ae89d2e4e9966503
Maxim Bortnikov <43994704+Northstrix@users.noreply.github.com>
2020-04-25 11:43:29 +0100

    Update ESP8266-based-9-channel-Wi-Fi-relay.ino

--- ESP8266-based-9-channel-Wi-Fi-relay.ino
+++ ESP8266-based-9-channel-Wi-Fi-relay.ino
@@ -1,6 +1,6 @@
 // ESP8266-based 9 channel Wi-Fi relay
 // Developed by Maxim Bortnikov
-// For more information visit: https://github.com/Northstrix/ESP8266-based-9-channel-Wi-Fi-relay
+// For more information and for the circuit of this device visit: https://github.com/Northstrix/ESP8266-based-9-channel-Wi-Fi-relay
 #include <ESP8266WiFi.h> 
 const char* ssid     = "Blackberry";  // Your SSID (Network's name)
 const char* password = "seasidehills99"; // Password from your network
